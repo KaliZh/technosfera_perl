4314d08eff49f35802c1c826a6e625f8ad76a0a1
KaliZh <kalijangeldy@gmail.com>
2017-03-10 00:12:37 +0300

    technosfera_perl

--- solutions/santa/blib/lib/SecretSanta.pm
+++ solutions/santa/blib/lib/SecretSanta.pm
@@ -1,81 +1,35 @@
 package SecretSanta;
 
 use 5.010;
-use strict;
+use strict; 
 use warnings;
 use DDP;
-calculate();
-sub el {
-    my ($ar) = @_;
-    if (ref($ar) eq "ARRAY") {
-        return $ar->[0];
-    } else {
-        return $ar;
-    }
-}
 sub calculate {
     my @members = @_;
-    #my @members = (['A', 'B'], ['C', 'D'], 'E', 'F', 'G');
-    #my @members = ('A', 'B', 'C');
     my @res;
-    my $sz = 0;
-    for my $i (0..$#members) {
-        if (ref($members[$i]) eq "ARRAY") {
-            #push @res, [$members[$i]->[0], ];
-            $res[$sz]->[0] = $members[$i]->[0];
-            if ($i + 1 == scalar @members) {
-                if ($i == 1 || $i == 0) {
-                    pop @res;
-                } else {
-                    $res[$sz]->[1] = el($members[0]);
-                    $sz++;
-                    #push @res, [ , el($members[0])]; 
-                }
-            } else {
-                $res[$sz]->[1] = el($members[$i + 1]);
-                $sz++;
-                #push @res, [ , el($members[$i + 1])];
-            }
-            #push @res, [$members[$i]->[1], ];
-            $res[$sz]->[0] = $members[$i]->[1];
-            if ($i + 1 == scalar @members) {
-                if ($i == 1 || $i == 0) {
-                    pop @res;
-                } else {
-                    $res[$sz]->[1] = el($members[0]);
-                    $sz++;
-                    #push @res, [ , el($members[0])]; 
-                }
-            } else {
-                $res[$sz]->[1] = el($members[$i + 1]);
-                $sz++;
-                #push @res, [ , el($members[$i + 1])];
-            }
-        } else {
-            #push @res, [$members[$i], ];
-            $res[$sz]->[0] = $members[$i];
-            if ($i + 1 == scalar @members) {
-                if ($i == 1 || $i == 0) {
-                    pop @res;
-                } else {
-                    $res[$sz]->[1] = el($members[0]);
-                    $sz++;
-                    #push @res, [ , el($members[0])]; 
-                }
-            } else {
-                $res[$sz]->[1] = el($members[$i + 1]);
-                $sz++;
-                #push @res, [ , el($members[$i + 1])];
-            }
-        }
-    }
 	# ...
 	#	push @res,[ "fromname", "toname" ];
 	# ...
-    for my $i (0..$#res) {
-        print "$res[$i]->[0] $res[$i]->[1]\n";
+    for my $var (@members) {
+        if (ref($var) eq "ARRAY") {
+            push @res,[$var->[0], ""];
+            push @res,[$var->[1], ""];
+        } else {
+            push @res,[$var, ""];
+        }
     }
-	return @res;
+    my ($i, $j);
+    $i = 0;
+    $j = 2;
+    while ($i < scalar @res) {
+        $res[$i]->[1] = $res[$j]->[0];
+        $i++;
+        $j++;
+        if ($j == scalar @res) {
+            $j = 0;
+        }
+    }
+    return @res;
 }
 
 1;
